053fcf8c81f18cf97f07927baa08a60d21232a7f
ad044 <antidepressant044@protonmail.com>
2021-12-20 22:48:04 +0400

    remove unused sound queue

--- src/sound.h
+++ src/sound.h
@@ -2,8 +2,6 @@
 
 #include "miniaudio.h"
 
-#define MAX_SOUND_QUEUE_SIZE 32
-
 typedef enum {
 	SND_110,
 	SND_111,
@@ -17,11 +15,6 @@ typedef enum {
 	SND_119,
 	SND_120,
 } SoundID;
-
-typedef struct {
-	int size;
-	SoundID arr[MAX_SOUND_QUEUE_SIZE];
-} SoundQueue;
 
 void play_sound(ma_engine *audio_engine, SoundID id);
 int sounds_init(ma_engine *audio_engine);
